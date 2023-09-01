ee27a81952672a878a4a8287a13ac10f4cd66880
Saad-054 <saad.siddiqui@msn.com>
2023-09-01 18:40:40 +0100

    debugging

--- starter/index.js
+++ starter/index.js
@@ -7,40 +7,40 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "Enter the title of your project."
+        message: "Enter the title of your project:"
     },
     {
         type: "input",
         name: "description",
-        message: "Enter a description of your project."
+        message: "Enter a description of your project:"
     },
     {
         type: "input",
         name: "installation",
-        message: "Enter installation instructions"
+        message: "Enter installation instructions:"
     },
     {
         type: "input",
         name: "usage",
-        message: "Enter usage information."
+        message: "Enter usage information:"
     },
     {
         type: "input",
-        name: "contribbuting",
-        message: "Enter contribbution guidelines."
+        name: "contributing",
+        message: "Enter contribution guidelines:"
     },
     {
         type: "input",
         name: "tests",
-        message: "Enter test instructions"
+        message: "Enter test instructions:"
     },
     {
         type: "list",
         name: "license",
-        message: "Chose a license for your application",
+        message: "Chose a license for your application:",
         choices: [
             "MIT",
-            "Apache, 2.0",
+            "Apache 2.0",
             "GPL 3.0",
             "ISC",
             "None"
@@ -64,9 +64,9 @@ function writeToFile(fileName, data) {
 
 function init() {
     inquirer.prompt(questions)
-    .them(answers =>) {
+    .then(answers =>) {
         const markdown = generateMarkdown(answers);
-        writeToFile("README.md", markdown");
+        writeToFile("README.md", markdown);
         console.log("README.md successfully generated!");
     })
     .catch(error => console.error(error));
