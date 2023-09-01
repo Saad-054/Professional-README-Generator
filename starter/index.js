426cccefa08abe41f6908dbe6f34f8cdeb3372f9
Saad-054 <saad.siddiqui@msn.com>
2023-09-01 18:26:55 +0100

    array questions for user in index.js

--- starter/index.js
+++ starter/index.js
@@ -7,6 +7,46 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "Enter the title of your project"
+        message: "Enter the title of your project."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information."
+    },
+    {
+        type: "input",
+        name: "contribbuting",
+        message: "Enter contribbution guidelines."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter test instructions"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose a license for your application",
+        choices: [
+            "MIT",
+            "Apache, 2.0",
+            "GPL 3.0",
+            "ISC",
+            "None"
+        ]
+    },
+    {
+        
     }
 ]
