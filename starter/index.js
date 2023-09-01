205786085ca7f356bc79c431e211a163299a9d85
Saad-054 <saad.siddiqui@msn.com>
2023-09-01 18:18:33 +0100

    initial index.js

--- starter/index.js
+++ starter/index.js
@@ -3,19 +3,10 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
 const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+    {
+        type: "input",
+        name: "title",
+        message: "Enter the title of your project"
+    }
+]
